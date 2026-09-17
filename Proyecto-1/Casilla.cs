a504491064ca2a31e8382a50279474ec657e4bf4
JHernandezGH07 <j.hernandez.14@estudiantec.cr>
2026-09-16 21:20:50 -0600

    Reorganizacion de Casilla para herencia y polimorfismo

--- Proyecto-1/Casilla.cs
+++ Proyecto-1/Casilla.cs
@@ -3,7 +3,8 @@ using System;
 // Clase Casilla:
 // Representa cada uno de los espacios físicos que conforman el tablero de Monopoly.
 // Cada objeto 'Casilla' se almacena dentro del campo 'data' de un 'Node' en la lista enlazada del tablero.
-public class Casilla
+// Clase abstracta que hereda a las clases Propiedad, CasillaEvento y CasillaEspecial.
+public abstract class Casilla
 {
     // Posición o índice numérico en el tablero (de 0 a TotalCasillas - 1).
     public int Posicion { get; set; }
@@ -21,35 +22,42 @@ public class Casilla
     // - "Suerte": Otorga eventos aleatorios (premios, multas, saltos).
     public string Tipo { get; set; }
 
-    // Precio de compra de la casilla si es de tipo "Propiedad".
-    public int Precio { get; set; }
-
-    // Monto de renta que debe pagar un jugador si cae en esta casilla y pertenece a otro jugador.
-    // En casillas de "Impuesto", representa el monto a pagar al fisco.
-    public int Renta { get; set; }
-
-    // Referencia al jugador que es dueño de esta propiedad.
-    // Si es null, significa que la propiedad está disponible para ser comprada.
-    public Jugador? Propietario { get; set; }
-
     // Constructor para inicializar una casilla con sus atributos básicos.
-    public Casilla(int posicion, string nombre, string tipo, int precio = 0, int renta = 0)
+    public Casilla(int posicion, string nombre, string tipo)
     {
         this.Posicion = posicion;
         this.Nombre = nombre;
         this.Tipo = tipo;
-        this.Precio = precio;
-        this.Renta = renta;
-        this.Propietario = null;
     }
+}
+public class Propiedad : Casilla
+{
+    // Precio de compra de la casilla si es de tipo "Propiedad".
+    int PrecioCompra { get; set; }
 
-    // Determina si la casilla es un bien inmueble que puede comprarse y generar rentas.
-    public bool EsPropiedad()
+    // Monto de renta que debe pagar un jugador si cae en esta casilla y pertenece a otro jugador.
+    // En casillas de "Impuesto", representa el monto a pagar al fisco.
+    int AlquilerBase { get; set; }
+
+    // Referencia al jugador que es dueño de esta propiedad.
+    // Si es null, significa que la propiedad está disponible para ser comprada.
+    Jugador? Propietario { get; set; }
+    
+    // Verifica si la propiedad se encuentra hipotecada
+    bool IsHipotecada { get; set; }
+
+    string ColorGrupo { get; set; }
+
+    public Propiedad(int posicion, string nombre, string tipo, int precioCompra, int alquilerBase, Jugador propietario, string colorGrupo) : base(posicion, nombre, tipo)
     {
-        return this.Tipo == "Propiedad";
+        this.PrecioCompra = precioCompra;
+        this.AlquilerBase = alquilerBase;
+        this.Propietario = propietario;
+        this.IsHipotecada = false;
+        this.ColorGrupo = colorGrupo;
     }
 
-    // Determina si esta propiedad ya fue adquirida por algún jugador de la partida.
+    // Verifica si la casilla tiene un propietario.
     public bool TienePropietario()
     {
         return this.Propietario != null;
@@ -61,11 +69,32 @@ public class Casilla
         if (this.Tipo == "Propiedad")
         {
             string dueño = this.Propietario != null ? this.Propietario.Nombre : "Sin dueño";
-            return $"[{this.Posicion}] {this.Nombre} (Propiedad) - Precio: ${this.Precio} | Renta: ${this.Renta} | Dueño: {dueño}";
+            return $"[{this.Posicion}] {this.Nombre} (Propiedad) - Precio: ${this.PrecioCompra} | Renta: ${this.AlquilerBase} | Dueño: {dueño}";
         }
         else
         {
             return $"[{this.Posicion}] {this.Nombre} ({this.Tipo})";
         }
+    }
+}
+public class CasillaEvento : Casilla
+{
+    string tipoEvento
+    public CasillaEvento(int posicion, string nombre, string tipo) : base(posicion, nombre, tipo)
+    {
+        
+    }
+
+    public void ObtenerEvento()
+    {
+        
+    }
+}
+public class CasillaEspecial : Casilla
+{
+    
+    public CasillaEspecial(int posicion, string nombre, string tipo) : base(posicion, nombre, tipo)
+    {
+        
     }
 }
